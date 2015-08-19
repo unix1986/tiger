d93054d2faa3dbcf24df561f2d4b347eaac71cbf
milo <unix1986@qq.com>
2015-08-19 23:23:25 +0800

    add zlog support

--- proj/main.cpp
+++ proj/main.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "thirdparty/zlog/include/zlog.h"
 
+const static char *zlog_conf = "/home/milo/tiger/proj/config/zlog.conf";
 int main(int argc, char *argv[]) {
-    printf("Hello Tiger!\n");
+    if (dzlog_init(zlog_conf, "default") != 0) {
+        fprintf(stderr, "dzlog_init error!\n");
+        exit(1);
+    }
+    dzlog_warn("Hello Tiger!");
     return 0;
 }
