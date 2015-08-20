6d69f1407b1bd957bb99f33bd1ba409a0abaeedd
unix1986 <unix1986@qq.com>
2015-08-20 10:23:30 +0800

    add zlog doc

--- proj/main.cpp
+++ proj/main.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "thirdparty/zlog/include/zlog.h"
 
-const static char *zlog_conf = "/home/milo/tiger/proj/config/zlog.conf";
+const static char *zlog_conf = "/home/wangjingjing/tiger/proj/config/zlog.conf";
 int main(int argc, char *argv[]) {
     if (dzlog_init(zlog_conf, "default") != 0) {
         fprintf(stderr, "dzlog_init error!\n");
