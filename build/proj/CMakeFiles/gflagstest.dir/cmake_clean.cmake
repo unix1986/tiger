1910341f59fbc28427ffcb3993a57f71efd2471d
unix1986 <unix1986@qq.com>
2015-09-07 15:39:05 +0800

    modify target_link_libraries

---
+++ build/proj/CMakeFiles/gflagstest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gflagstest.dir/gflagstest.o"
+  "../bin/gflagstest.pdb"
+  "../bin/gflagstest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gflagstest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/proj/CMakeFiles/tiger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tiger.dir/main.o"
+  "../bin/tiger.pdb"
+  "../bin/tiger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tiger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
