6af5366ce9e953d7a3ab3c5d57d264702b1bfbd9
unix1986 <unix1986@qq.com>
2015-09-08 00:41:30 +0800

    modify protobuf lib

---
+++ build/proj/CMakeFiles/MYLS.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MYLS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/MYLS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
