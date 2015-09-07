c311f82b71d93da48dbaf1838641db3b15c06121
unix1986 <unix1986@qq.com>
2015-09-07 23:57:14 +0800

    tiger common add zlog support

--- proj/template.cpp
+++ proj/template.cpp
@@ -6,6 +6,5 @@ DEFINE_string(test_str, "hello tiger!", "gflags test");
 
 int tiger_main(int argc, char *argv[]) {
     dzlog_warn("%s", FLAGS_test_str.c_str());
-    printf("%s\n", FLAGS_test_str.c_str());
     return 0;
 }
